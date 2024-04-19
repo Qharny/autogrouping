8918b6c2937ad9e01a838020f38537e7a3be8484
Qharny <cannycc1020@gmail.com>
2024-04-19 17:54:20 +0000

    modified lib file

--- lib/autogrouping.dart
+++ lib/autogrouping.dart
@@ -2,39 +2,81 @@
 //   return 6 * 7;
 // }
 
+import 'dart:ffi';
 import 'dart:io';
 
 void main(List<String> args) {
   prompt();
 }
 
-void prompt() {
-  print("Select your Input choice: \n");
-  print("-----------\n");
-  print(
-      "1. Read External file \n2. Input Characters \n3. Input numbers \n4. exit");
-  print("\nSelect 1, 2, 3 or 4: ");
+prompt() {
+  // Prompt user to select choice
+  stdout.write('AUTO GROUPING\n'
+      '\n'
+      'Select the data type:\n'
+      '1. File (Read from an external file)\n'
+      '2. Strings\n'
+      '3. Numbers/Integers\n'
+      'Enter your choice: ');
+
   // get user input
-  var input = int.parse(stdin.readLineSync() ?? '');
-  // condition to select chose
-  if (input == 1) {
-    // get external file and red content
-    try {
-      File file =
-          File("C://Users//MR_KABUTEYY//Desktop//test.txt"); // get file path
-      print("Do you want to see file content? \n Y for Yes and N for No");
-      var readContent = stdin.readLineSync();
-      if (readContent?.toLowerCase() == 'y') {
-        String contents = file.readAsStringSync();
-        print(contents);
-      } else if (readContent?.toLowerCase() == "n") {
-        print("Continuing proccess");
-      }
-    } catch (e) {
-      print("Error reading file $e");
-    }
+  var choice = int.parse(stdin.readLineSync() ?? '');
+
+  List<dynamic> inputData = []; // List to store the data
+  // a switch case to get choice
+  switch (choice) {
+    case 1:
+      inputData = readFromFile();
+      break;
+    case 2:
+      inputData = readStrings();
+      break;
+    case 3:
+      inputData = readNumbers();
   }
-  if (input == 2) {}
-  if (input == 3) {}
-  if (input == 4) {}
+}
+
+// function for reading file
+List<dynamic> readFromFile() {
+  // Read data from an external file
+  stdout.write('Enter the name of the file: ');
+  var fileName = stdin.readLineSync()!;
+
+  // error handling for the file
+  try {
+    var file = File(fileName);
+    var data = file.readAsStringSync();
+    return data.split(',');
+  } catch (e) {
+    print('Error: $e');
+    return [];
+  }
+}
+
+// function for acceoting strings
+List<String> readStrings() {
+  List<String> strings = []; // list to store Strings
+  while (true) {
+    stdout.write('Enter the strings (type exit to stop): ');
+    var input = stdin.readLineSync()!;
+    if (input == 'exit') {
+      break;
+    }
+    strings.add(input);
+  }
+  return strings;
+}
+
+// function for accepting intergers
+List<Int> readNumbers() {
+  List<Int> numbers = []; // list to store numbers
+  while (true) {
+    stdout.write('Enter the numbers (type 0 to stop): ');
+    int inputNum = int.parse(stdin.readLineSync()!);
+    if (inputNum == 0) {
+      break;
+    }
+    numbers.add(inputNum as Int);
+  }
+  return numbers;
 }
